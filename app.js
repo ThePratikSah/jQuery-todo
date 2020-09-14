bf081eaf9fd4096e3727c5b404f596f9549baf95
Pratik Sah <shivam3448@gmail.com>
2020-09-14 14:40:43 +0530

    Both add and remove are working

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 $(document).ready(function () {
-
+  let i = 0;
   // logic for adding to the todos list
-  $('input').on('keypress', (e) => {
-    if (e.which == 13 && $('input').val() != '') {
-      let todo = $('input').val();
-      $('input').val("");
-      $('.incomplete').append("<div class='card'>" + todo + "<i class='fas fa-trash'></i></div>");
+  $("input").on("keypress", (e) => {
+    if (e.which == 13 && $("input").val() != "") {
+      let todo = $("input").val();
+      $("input").val("");
+      $(".incomplete").append(
+        "<div class='card' onclick='removeTodo(this);' id='a" + i + "'>" + todo + "</div>");
+      i++;
     }
-  })
-
-  // logic for removing from the todos list
-  $("<i class='fas fa-trash'></i>").click(() => {
-    let itemToDelete = $(this).parent();
-    itemToDelete.fadeOut(() => {
-      itemToDelete.remove();
-    });
   });
 });
+
+// logic for removing from the todos list
+let removeTodo = (a) => {
+  $(a).remove();
+};
