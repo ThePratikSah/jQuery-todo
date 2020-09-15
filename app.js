770ae169c756f6a1fcd3a66b36d0163fbba17ce6
Pratik Sah <shivam3448@gmail.com>
2020-09-15 21:25:43 +0530

    Made it Progressive Web App

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ checkTodos();
 
 function checkTodos() {
   // todos in the key of my localStorage
-  let dataInLocalStorage = localStorage.getItem('todos');
+  let dataInLocalStorage = localStorage.getItem("todos");
   // check if it is null or not
   if (dataInLocalStorage == null) {
     todos = [];
@@ -14,33 +14,37 @@ function checkTodos() {
   let html = "";
   todos.forEach((todo, index) => {
     html += `<div class='card' onclick='removeTodo(${index});'>${todo}</div>`;
-  })
-  $('.incomplete').empty().append(html);
+  });
+  $(".incomplete").empty().append(html);
 }
 
 // adding items in todos
 
-$('input').on('keypress', e => {
-  if (e.which === 13 && $('input').val() !== '') {
-    todo = $('input').val();
-    let todosData = localStorage.getItem('todos');
+$("input").on("keypress", (e) => {
+  if (e.which === 13 && $("input").val() !== "") {
+    todo = $("input").val();
+    let todosData = localStorage.getItem("todos");
     if (todosData == null) {
       todos = [];
     } else {
       todos = JSON.parse(todosData);
     }
     todos.push(todo);
-    localStorage.setItem('todos', JSON.stringify(todos));
-    $('input').val('');
+    localStorage.setItem("todos", JSON.stringify(todos));
+    $("input").val("");
     checkTodos();
   }
-})
+});
 
 // logic for removing from the todos list
 let removeTodo = (index) => {
-  let todosData = localStorage.getItem('todos');
+  let todosData = localStorage.getItem("todos");
   todos = JSON.parse(todosData);
   todos.splice(index, 1);
-  localStorage.setItem('todos', JSON.stringify(todos));
+  localStorage.setItem("todos", JSON.stringify(todos));
   checkTodos();
 };
+
+// Registering the service worker
+navigator.serviceWorker &&
+  navigator.serviceWorker.register("SW.js").then(function (registration) {});
