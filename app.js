86d7d47b78264181331d978d97ccde05c4bc8bd4
Pratik Sah <shivam3448@gmail.com>
2020-09-15 20:13:50 +0530

    Added local storage support

--- app.js
+++ app.js
@@ -1,18 +1,46 @@
-$(document).ready(function () {
-  let i = 0;
-  // logic for adding to the todos list
-  $("input").on("keypress", (e) => {
-    if (e.which == 13 && $("input").val() != "") {
-      let todo = $("input").val();
-      $("input").val("");
-      $(".incomplete").append(
-        "<div class='card' onclick='removeTodo(this);' id='a" + i + "'>" + todo + "</div>");
-      i++;
+checkTodos();
+
+// check for data in localStorage
+
+function checkTodos() {
+  // todos in the key of my localStorage
+  let dataInLocalStorage = localStorage.getItem('todos');
+  // check if it is null or not
+  if (dataInLocalStorage == null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(dataInLocalStorage);
+  }
+  let html = "";
+  todos.forEach((todo, index) => {
+    html += `<div class='card' onclick='removeTodo(${index});'>${todo}</div>`;
+  })
+  $('.incomplete').empty().append(html);
+}
+
+// adding items in todos
+
+$('input').on('keypress', e => {
+  if (e.which === 13 && $('input').val() !== '') {
+    todo = $('input').val();
+    let todosData = localStorage.getItem('todos');
+    if (todosData == null) {
+      todos = [];
+    } else {
+      todos = JSON.parse(todosData);
     }
-  });
-});
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+    $('input').val('');
+    checkTodos();
+  }
+})
 
 // logic for removing from the todos list
-let removeTodo = (a) => {
-  $(a).remove();
+let removeTodo = (index) => {
+  let todosData = localStorage.getItem('todos');
+  todos = JSON.parse(todosData);
+  todos.splice(index, 1);
+  localStorage.setItem('todos', JSON.stringify(todos));
+  checkTodos();
 };
